13e1342a0c6aa638ca57987b1506fe6c45639b63
Will Metivier <willnolin@gmail.com>
2021-07-26 17:20:25 -0400

    first dev push, mobile icons

--- client/src/controlled_components/CreateLocation.jsx
+++ client/src/controlled_components/CreateLocation.jsx
@@ -56,18 +56,23 @@ export default function CreateLocation() {
         <label className="form-field">Name:
           <input type="text" name="name" value={name} onChange={handleChange} />
         </label>
+        <br />
         <label className="form-field">Street:
           <input type="text" name="street" value={street} onChange={handleChange} />
         </label>
+        <br />
         <label className="form-field">City:
           <input type="text" name="city" value={city} onChange={handleChange} />
         </label>
+        <br />
         <label className="form-field">State:
           <input type="text" name="state" value={state} onChange={handleChange} />
         </label>
+        <br />
         <label className="form-field">Image:
           <input type="text" name="img_url" value={img_url} onChange={handleChange} />
         </label>
+        <br />
         <label className="form-field">Message:
           <textarea name="message" rows="5" value={message} onChange={handleChange} />
         </label>
@@ -81,7 +86,9 @@ export default function CreateLocation() {
         <label className="form-field">Commission:
           <input type="number" name="commission" value={commission} onChange={handleChange} />
         </label>
+        <br />
         <button>Submit</button>
+        <br />
       </form>
     </div>
   )
