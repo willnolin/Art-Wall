761fae7fdbfa50a324b1488edaf882bf51fa74c1
Will Metivier <willnolin@gmail.com>
2021-08-13 11:44:59 -0400

    landing page design updates

--- client/src/screens/Landing.jsx
+++ client/src/screens/Landing.jsx
@@ -19,15 +19,33 @@ export default function Landing() {
             <p>Click "View / Edit Profile" to get started</p>
             </>
 }
-      <div className="landing-row">
+      <div className="landing-row first">
+        <div className= "top-row-content">
+        <div className="top">
+          <p className="welcome-section-title"> WELCOME TO ART WALL!</p>
+          <p className="welcome-section"> a community for artists and enthusiants</p>
+          </div>
         <img src={paintbrush} alt="paintbrush" className="paintbrush" />
-        <div>
-          <p className="welcome-section-title"> Welcome to Art Wall!</p>
-          <p className="welcome-section">We are a community of artists looking for spaces
-            to hang and sell art! </p>
-        </div>
+          </div>
       </div>
-      <div className="landing-row">
+      <div className="landing-row second">
+      <div className= "second-row-content">
+        <div className="featured-artist-card ">
+          <div className="featured-artist-overlay ">
+        {/* <img src="https://res.cloudinary.com/willnolin/image/upload/v1627478256/paintbrushes_usygaj.jpg"
+          alt="paintbrushes"  className="paintbrushes" /> */}
+      <p className= "featured-artist">
+              Artist Of The Week:
+              Molly Parks
+        <br/>
+
+            </p>
+            </div>
+          </div>
+        <div className="call-to-action-card">
+          <div className="call-to-action-overlay">
+        <img src="https://res.cloudinary.com/willnolin/image/upload/v1627478256/paintbrushes_usygaj.jpg"
+          alt="paintbrushes"  className="paintbrushes" />
       <p className= "call-to-action">
         You can search for registered hosts in your area.
       Register an account and contact any of our listed hosts! Fill out your artist profile!
@@ -35,9 +53,10 @@ export default function Landing() {
         local spaces so what are you waiting for?
         <br/>
         <Link to="/register" className="sign-up-link"> Sign up TODAY! </Link>
-        </p>
-        <img src="https://res.cloudinary.com/willnolin/image/upload/v1627478256/paintbrushes_usygaj.jpg"
-          alt="paintbrushes"  className="paintbrushes" />
+            </p>
+            </div>
+          </div>
+          </div>
       </div>
     </div>
     // </Layout>
