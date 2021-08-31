18536ca084a8d548732ad112337e165d7ef75616
Will Metivier <willnolin@gmail.com>
2021-08-31 12:36:39 -0400

    mailer url

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-<<<<<<< HEAD
-  default from: 'from@example.com'
-=======
   default from: 'willnolin@gmail.com'
->>>>>>> cdcb2a9053261c9036ed879be6c16025de9a0ae7
   layout 'mailer'
 end
