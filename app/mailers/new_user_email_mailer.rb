eb3e6a4f78e9e6599c35760ef185ad1e79376f2b
Will Metivier <willnolin@gmail.com>
2021-09-01 15:56:04 -0400

    env

--- app/mailers/new_user_email_mailer.rb
+++ app/mailers/new_user_email_mailer.rb
@@ -1,6 +1,6 @@
 class NewUserEmailMailer < ApplicationMailer
   def notify_user(user)
     @user = user
-    mail(to: @user.email, from: ENV[GMAIL_USERNAME], subject: 'Welcome to Art Wall')
+    mail(to: @user.email, from: ENV['GMAIL_USERNAME'], subject: 'Welcome to Art Wall')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: ENV['DOMAIN'],
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
