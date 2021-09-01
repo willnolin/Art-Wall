bc621e9defb4f96a5b71827a84aa1048eec743ec
Will Metivier <willnolin@gmail.com>
2021-09-01 16:29:24 -0400

    app

--- app/mailers/new_user_email_mailer.rb
+++ app/mailers/new_user_email_mailer.rb
@@ -1,4 +1,5 @@
 class NewUserEmailMailer < ApplicationMailer
+  default from: ENV['GMAIL_USERNAME']
   def notify_user(user)
     @user = user
     mail(to: @user.email, subject: 'Welcome to Art Wall')
