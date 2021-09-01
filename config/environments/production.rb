781fcf837c4653d4f46a5fcd9f88aead5b3d0cbe
Will Metivier <willnolin@gmail.com>
2021-09-01 14:44:22 -0400

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'art-wall-api.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'art-wall-api.herokuapp.com', protocol: 'https',
+                                               only_path: false }
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
